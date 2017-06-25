8b0872c308a2e5f7219dcdbf9507ce158f0c5719
Carlos Gomes <kmartinho8@gmail.com>
2017-06-25 14:36:42 +0200

    removed unused code

--- build.sh
+++ build.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# cwd=$(pwd)
-# 
-# # build the SLAM engine
-# cd thirdparty/slam_engine/ORB_SLAM2/
-# sh build.sh
-# cd $cwd
-
 # build the wrapper library
 rm -rf build/
 mkdir build
 cd build
 cmake ..
-make
+make -j

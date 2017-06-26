0fb040ef0065f0adde9bfe7a7b1f31aecb5e0ed0
Carlos Gomes <kmartinho8@gmail.com>
2017-06-26 18:35:23 +0200

    moved some files arround, still not compiling correctly

---
+++ build_ros.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+root_dir=$(pwd)
+
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:$root_dir/src/app/ros
+
+# build the wrapper library
+rm -rf build/
+mkdir build
+cd build
+cmake .. -DBUILD_ROS_BINDING=TRUE -DBUILD_TESTS=FALSE -DBUILD_APP=TRUE
+make -j

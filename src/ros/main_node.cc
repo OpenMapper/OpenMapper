99da7c81cafed6c3fc838c0a8830981ef415fcc3
Carlos Gomes <kmartinho8@gmail.com>
2017-06-27 09:49:09 +0200

    style fixes and combination of ros/app/gtests compilation

--- src/ros/main_node.cc
+++ src/ros/main_node.cc
@@ -56,17 +56,14 @@ int main(int argc, char** argv) {
 }
 
 void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg) {
-      // Copy the ros image message to cv::Mat.
-      cv_bridge::CvImageConstPtr cv_ptr;
-      try
-      {
-          cv_ptr = cv_bridge::toCvShare(msg);
-      }
-      catch (cv_bridge::Exception& e)
-      {
-          ROS_ERROR("cv_bridge exception: %s", e.what());
-          return;
-      }
+  // Copy the ros image message to cv::Mat.
+  cv_bridge::CvImageConstPtr cv_ptr;
+  try {
+    cv_ptr = cv_bridge::toCvShare(msg);
+  } catch (cv_bridge::Exception& e) {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
 
-      mpSLAM->TrackMonocular(cv_ptr->image,cv_ptr->header.stamp.toSec());
+  mpSLAM->TrackMonocular(cv_ptr->image, cv_ptr->header.stamp.toSec());
 }

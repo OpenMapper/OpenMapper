19c1aa62ddd9c0822ae6cbf7cb790248be2baf73
Carlos Gomes <kmartinho8@gmail.com>
2017-07-03 15:00:00 +0200

    not using the findGlog.cmake

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+rm -rf build/
+# rm -rf bin/
+# rm -rf lib/
+
 # Build OpenMapper Static Lib only
 mkdir -p build
 cd build
-cmake ..  -DBUILD_TESTS=TRUE -DGLOG_ROOT_DIR=../../thirdparty/glog
+cmake ..  -DBUILD_TESTS=TRUE
 make -j8

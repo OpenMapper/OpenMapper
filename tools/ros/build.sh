4af1ae913a42e24daa3340f59976bd8681ad5d74
Carlos Gomes <kmartinho8@gmail.com>
2017-07-03 16:53:51 +0200

    smaller fixes in order to compile under ubuntu with ROS again.

--- tools/ros/build.sh
+++ tools/ros/build.sh
@@ -6,6 +6,6 @@ export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:$root_dir
 
 # Build OpenMapper ROS Node
 mkdir -p ros_build
-cd build
-cmake ../wrapper/ros/  -DCMAKE_BUILD_TYPE=DEBUG 
+cd ros_build
+cmake ../wrapper/ros/  -DCMAKE_BUILD_TYPE=DEBUG
 make -j4

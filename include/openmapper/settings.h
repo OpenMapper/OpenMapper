52bfae773af2cb6dac6c03bb351aa31c04f5c26c
Carlos Gomes <kmartinho8@gmail.com>
2017-07-09 21:38:43 +0200

    fixed correct path to BoW and settings

---
+++ include/openmapper/settings.h
@@ -0,0 +1,19 @@
+// Auto-generated headers file, do not edit, will be overwritten by cmake. 
+// (c) 2017 OpenMapper
+
+#ifndef INCLUDE_OPENMAPPER_CONFIG_H_
+#define INCLUDE_OPENMAPPER_CONFIG_H_
+
+#include <string>
+
+const std::string path_to_vocabulary =
+    "/PATH/thirdparty/slam_engine/ORB_SLAM2/Vocabulary/ORBvoc.txt";
+const std::string path_to_settings =
+    "/PATH/thirdparty/slam_engine/ORB_SLAM2/Vocabulary/iphone.yaml";
+
+const std::string static_video = "/PATH/thirdparty/slam_engine/ORB_SLAM2/Vocabulary/test_data/static.mov";
+const std::string dynamic_video = "/PATH/thirdparty/slam_engine/ORB_SLAM2/Vocabulary/test_data/dynamic.mov";
+
+
+#endif  // INCLUDE_OPENMAPPER_CONFIG_H_
+
